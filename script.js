989ee6afa809c1da9843e50cd28be6c065a7ba69
Jose <jm.bebor@gmail.com>
2025-05-23 09:56:38 +0000

    Hecho

--- script.js
+++ script.js
@@ -30,7 +30,23 @@ window.onload = () => {
   document.getElementById("dataset").textContent = JSON.stringify(marketplaceData, null, 2);
 
   // Ejercicio pendiente por resolver: resultado vacío
-  const result = [];
+  const result = marketplaceData.map(seller => ({
+    //...seller
+    sellerId: seller.sellerId,
+    sellerName: seller.sellerName,
+    orders: seller.orders.map(order => ({
+      orderId: order.orderId,
+      customer: order.customer,
+      items: order.items.map(item => ({
+        productId: item.productId,
+        title: item.title,
+        variant: item.variant.sku,
+        price: "$"+ item.variant.price,
+        quantity: item.quantity,
+        subtotal:"$"+ item.variant.price * item.quantity
+      }))
+    }))
+  }));
 
   // Mostrar resultado
   document.getElementById("output").textContent = JSON.stringify(result, null, 2);
